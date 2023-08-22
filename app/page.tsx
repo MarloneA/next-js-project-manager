45d9e84f9159251ce67dd8d320004fd948e2ba6f
Marlone Akidiva <marlone.akidiva@clearlabs.com>
2023-08-23 01:16:40 +0300

    feature React DnD

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <Header />
-      <Board/>
+      <Board />
     </main>
   )
 }
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,17 +1,32 @@
+import { databases } from '@/appwrite';
 import { getTodosGroupedByColumn } from '@/lib/getTodosGroupedByColumn';
 import { create } from 'zustand'
 
 interface BoardStore {
   board: Board;
-  getBoard: () => void
+  getBoard: () => void,
+  setBoardState: (board: Board) => void;
+  updateTodoInDB: (todo: Todo, columnId: TypedColumn) => void;
 }
 
 export const useBoardStore = create<BoardStore>((set) => ({
   board: {
-    columns: new Map<TypedColumn, Column>()
+    columns: new Map<TypedColumn, Column>(),
   },
   getBoard: async () => {
     const board = await getTodosGroupedByColumn();
     set({ board })
+  },
+  setBoardState: (board) => set({ board }),
+  updateTodoInDB: async (todo, columnId) => {
+    await databases.updateDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_COLLECTIONS_ID!,
+      todo.$id,
+      {
+        title: todo.title,
+        status: columnId
+      }
+    )
   }
 }))
